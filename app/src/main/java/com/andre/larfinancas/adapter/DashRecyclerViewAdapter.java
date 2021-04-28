3bee9b78da2440ae4ce697589473dc880a860724
Andre Carlos <andre.andresinho2009@hotmail.com>
2021-04-28 00:22:25 -0300

    Alterado style e paleta de cores do app

---
+++ app/src/main/java/com/andre/larfinancas/adapter/DashRecyclerViewAdapter.java
@@ -0,0 +1,4 @@
+package com.andre.larfinancas.adapter;
+
+public class DashRecyclerViewAdapter {
+}
---
+++ app/src/main/java/com/andre/larfinancas/viewholder/DashboardViewHolder.java
@@ -0,0 +1,13 @@
+package com.andre.larfinancas.viewholder;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DashboardViewHolder extends RecyclerView.ViewHolder {
+
+    public DashboardViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
